0c2f9583b14cb917ced13c4ee4741a75695c0984
Shimozono Misato <>
2018-11-22 18:54:12 +0900

    $ django-admin startproject manager_project

---
+++ zvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/osato/projects/private/python/django_manager_project/zvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
